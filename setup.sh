77a2f47995d3b04b5ca2a2d58fe09221058d99c6
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-10-25 00:35:21 +0100

    Add basic setup functionality

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
-for file in $(ls -A)
-do
-  echo $file
+#!/usr/local/bin/bash
+# Symlink dotfiles into home folder
+
+dir=$PWD
+backup_dir=$dir/backup
+dotfiles=".bashrc .bash_profile .vimrc .tmux.conf" 
+
+if [ ! -d $backup_dir ]; then
+  mkdir $backup_dir
+fi
+
+for file in $dotfiles; do
+  # If the file already exists, move it to a backup folder
+  if [ -f ~/$file ]; then
+    mv ~/$file $backup_dir
+  fi
+  # If a Symlink doesn't exist, symlink the .dotfile
+  if [ ! -L ~/$file ]; then
+    ln -s $PWD/$file ../
+    echo "Symlinked $file"
+  fi
 done
